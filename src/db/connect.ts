fd8b420fb25e71939290ef4c07c3403fb536c535
fierylion <mawalladaniel2018@gmail.com>
2023-07-25 21:40:27 +0300

    add: create a code structure

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -3,7 +3,5 @@ const connectDB =  (url:string) => {
  return mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true, 
-    useFindAndModify: false,
-    useCreateIndex: true
  } as mongoose.ConnectOptions)};
 export default connectDB;
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,30 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './db/connect';   
-dotenv.config();
+dotenv.config()
 const app = express();
+app.use(express.json());
 
-try{
-    const connectToDatabase =async () => {
-        await connectDB(process.env.MONGO_URI!);
-        console.log('Database Connected');
-        app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}....`))
-        
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const port:number =4000;
+const start =async () => {
+    try{
+        const mongo_uri = process.env.MONGO_URI;
+        if(mongo_uri){
+            await connectDB(mongo_uri);
+            console.log('Database Connected');
+            app.listen(port, () => console.log(`Server running on port ${port}....`))
+        }
+        else{
+            throw new Error('Cant connect to the database!, MONGO URI not provided!')
+        }
     }
+    catch(err){
+        console.log(err);
+    }
+
 }
-catch(err){
-    console.log(err);
-}
+start()
